45ba47271297a84582024cc55ed06880f47d74de
Victor Ughonu <victorughonu95@gmail.com>
2018-12-02 18:04:13 +0100

    unsure of whats happening

--- requester/__init__.py
+++ requester/__init__.py
@@ -1,8 +1,14 @@
+from requester import views
+from wtforms import validators
+from wtforms.ext.appengine.db import model_form
+from flask_wtf import FlaskForm, Form
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from requester.features.forms import FeatureForm
+# from requester.features.forms import FeatureForm
+
+
 # Initialize app
 app = Flask(__name__)
 # Configure app from settings attached to the environmet variable loaded from the .env file or production environment variable
@@ -10,27 +16,35 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 
+
 # initialise database
-
 db = SQLAlchemy(app)
-
 # Add bootstrap
 Bootstrap(app)
-
 # import the application views
-
-from requester import views
-
-
 # Register Blueprints
-
-
 
 
 # Home Page route
 @app.route('/')
 @app.route('/home')
 def index():
+
+    form = FeatureForm(request.form)
+
+    # print(form.errors)
+    if request.method == 'POST':
+        title=request.form['title']
+        description=request.form['description']
+        client_priority=request.form['client_priority']
+        target_date = request.form['target_date']
+        client_id = request.form['client_id']
+        product_areas = request.form['product_areas']
+        
+        if form.validate():
+           pass
+        else:
+            flash('Error: All Fields are Required')
     return render_template('index.html')
 
 
--- requester/features/forms.py
+++ requester/features/forms.py
@@ -1,10 +1,26 @@
-from flask_wtf import FlaskForm, Form
-from wtforms.ext.appengine.db import model_form
-from wtforms import validators
-from requester.models import Feature
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField, DateField, SelectField, SelectMultipleField
+from wtforms.validators import DataRequired
 
-FeatureForm = model_form(Feature, Form, field_args={
-    'name': {
-        'validators': [validators.Length(max=10)]
-    }
-})
+
+class FeatureForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    client_priority = IntegerField('Client Priority')
+    target_date = DateField('Target Date')
+    client_id = SelectField('Client', coerce=int)
+    product_areas = SelectMultipleField('Product Areas')
+
+
+
+
+""" Possible view function for  FeatureForm 
+
+
+def edit_user(request, id):
+    user = User.query.get(id)
+    form = UserDetails(request.POST, obj=user)
+    form.group_id.choices = [(g.id, g.name)
+                              for g in Group.query.order_by('name')]
+
+"""
