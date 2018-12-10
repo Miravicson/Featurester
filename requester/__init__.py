0ed3d5417d7460b6fef78f27ced5069276d6041f
Victor Ughonu <victorughonu95@gmail.com>
2018-12-10 18:07:40 +0100

    added forms for products and clients

--- requester/__init__.py
+++ requester/__init__.py
@@ -1,57 +1,87 @@
-from wtforms import validators
-from wtforms.ext.appengine.db import model_form
-from flask_wtf import FlaskForm, Form
 import os
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-import datetime
+from flask_moment import Moment
 
 # Initialize app
 app = Flask(__name__)
-# Configure app from settings attached to the environmet variable loaded from the .env file or production environment variable
+# Configure app from settings attached to the environment variable loaded from the .env
+#  file or production environment variable
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 # initialise database
 db = SQLAlchemy(app)
 # Add bootstrap
 Bootstrap(app)
+
+# Add Flask Moment
+moment = Moment(app)
+
 # import the application views
 from requester import views
+
 #  Import the models
 from requester.models import Feature, Client, ProductArea
-# Import the from
-from requester.features.forms import FeatureForm
+
+# Import the form
+from requester.features.forms import FeatureForm, ClientForm, ProductAreaForm
+
+
 # Home Page route
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def index():
-        clients = Client.query.limit(3).all()
-        products = ProductArea.query.limit(5).all()
-        features = Feature.query.all()
-        form = FeatureForm()
-        form.client_id.choices= [(c.id, c.name) for c in clients]
-        form.product_areas.choices = [(p.id, p.name) for p in products]
+    clients = Client.query.limit(3).all()
+    products = ProductArea.query.order_by('id').limit(5).all()
+    features = Feature.query.order_by('id').limit(5).all()
+    form = FeatureForm()
+    form.client_id.choices = [(c.id, c.name) for c in clients]
+    form.product_areas.choices = [(p.id, p.name) for p in products]
+    form2 = ClientForm()
+    form3 = ProductAreaForm()
 
-        if form.validate_on_submit():
-                data = {}
-                data["title"] = form.title.data
-                data["description"] = form.description.data
-                data["target_date"] = form.target_date.data
-                data['client_priority'] = form.client_priority.data
-                client_id = form.client_id.data
-                data["client"] = Client.query.get(int(client_id))
-                product_areas = form.product_areas.data
-                product_areas = [ProductArea.query.get(int(i)) for i in product_areas]
-                f = Feature(**data)
-                for product_area in product_areas:
-                        product_area.features.append(f)
-                db.session.add(f)
-                db.session.commit()
-                return redirect(url_for('index'))
-        else:
-                flash(form.errors)
-        return render_template('index.html', product_list = products, clients=clients, features=features, form=form)
+    if form.validate_on_submit():
+        data = dict()
+        data["title"] = form.title.data
+        data["description"] = form.description.data
+        data["target_date"] = form.target_date.data
+        data['client_priority'] = form.client_priority.data
+        client_id = form.client_id.data
+        data["client"] = Client.query.get(int(client_id))
+        product_areas = form.product_areas.data
+        product_areas = [ProductArea.query.get(int(i)) for i in product_areas]
+        f = Feature(**data)
+        for product_area in product_areas:
+            product_area.features.append(f)
+        db.session.add(f)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        flash(form.errors)
 
+    if form2.validate_on_submit():
+        data = dict()
+        data["name"] = form2.name.data
+        data["email"] = form2.email.data
+        c = Client(**data)
+        db.session.add(c)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        flash(form.errors)
 
+    if form3.validate_on_submit():
+        data = dict()
+        data["name"] = form3.name.data
+        p = ProductArea(**data)
+        db.session.add(p)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        flash(form.errors)
 
+    return render_template('index.html', product_list=products, clients=clients, features=features, form=form,
+                           form2=form2, form3=form3)
--- requester/features/forms.py
+++ requester/features/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, IntegerField, DateField,SubmitField, SelectField, SelectMultipleField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, IntegerField, DateField, SubmitField, SelectField, SelectMultipleField
+from wtforms.validators import DataRequired, Email
 
 
 class FeatureForm(FlaskForm):
@@ -9,5 +9,17 @@ class FeatureForm(FlaskForm):
     client_priority = IntegerField('Client Priority')
     target_date = DateField('Target Date')
     client_id = SelectField('Client', choices=[], coerce=int)
-    product_areas = SelectMultipleField('Product Areas', choices=[], coerce=int)
+    product_areas = SelectMultipleField(
+        'Product Areas', choices=[], coerce=int)
     submit = SubmitField('Add Request')
+
+
+class ClientForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Create Client')
+
+
+class ProductAreaForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Add Product Areas')
